26ad99c9be56e8243ba2258d79a52522511ed2b9
bhavesh krishan garg <bhaveshgarg2005@gmail.com>
2021-05-26 13:18:03 +0530

    bhavesh

---
+++ Lab 6/ci.m
@@ -0,0 +1,4 @@
+﻿function compound_interest=ci(p,r,n,t)
+ci= p*(1+r/n)^(n*t)
+end
+fprintf("The compound interest = %d",ci);
---
+++ Lab 6/peri.m
@@ -0,0 +1,4 @@
+﻿function triangle=peri(a,b,c)
+peri=(a+b+c)/3
+end
+printf("The perimeter of triangle is = %f",peri);
